c5c0575fa44653d520b44750c3b6607e06868db0
Emran Atrooz <emranatrooz03@example.com>
2026-03-03 06:48:30 +0300

    Added update user endpoint

--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,14 +1,20 @@
 package com.codewithmosh.store.mappers;
 
 import com.codewithmosh.store.dtos.CreateUserRequest;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.dtos.UserDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    //@Mapping(target = "createdAt", expression = "java(java.time.LocalDate.now())")
     UserDto userToUserDto(User user);
+
     User toEntity(CreateUserRequest request);
+
+    void update(UpdateUserRequest request, @MappingTarget User user);
+    // MapStruct doesn't create new user object it just updates the EXISTING user object
+
 }
