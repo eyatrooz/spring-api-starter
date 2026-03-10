aee46859577fb8a4b0b9a7a4f3ff1ab47e34bc18
Emran Atrooz <emranatrooz2003@gmail.com>
2026-03-10 05:08:09 +0300

    Modified / Custom validation & Business Role validation for email

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -6,15 +6,19 @@ import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -27,33 +31,40 @@ public class UserController {
 
     @GetMapping()
     public Iterable<UserDto> getAllUsers(
-          @RequestParam(name = "sort", required = false, defaultValue = "") String sortBy){
+            @RequestParam(name = "sort", required = false, defaultValue = "") String sortBy) {
 
-        if(!Set.of("name", "email").contains(sortBy)){
+        if (!Set.of("name", "email").contains(sortBy)) {
             sortBy = "id";
         }
-       return userRepository.findAll(Sort.by(sortBy))
-               .stream()
-               .map(userMapper::userToUserDto)
-               .toList();
+        return userRepository.findAll(Sort.by(sortBy))
+                .stream()
+                .map(userMapper::userToUserDto)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity <UserDto> getUser(@PathVariable Long id){
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
 
-        if(user == null){
+        if (user == null) {
             return ResponseEntity.notFound().build();
         }
-           return ResponseEntity.ok(userMapper.userToUserDto(user));
+        return ResponseEntity.ok(userMapper.userToUserDto(user));
         //var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
         //var userDto = userMapper.userToUserDto(user);
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserRequest request, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<?> createUser(
+            @Valid @RequestBody CreateUserRequest request,
+            UriComponentsBuilder uriBuilder
+    ) {
+        if(userRepository.existsByEmail(request.getEmail())){
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email is already exist") );
+        }
 
-        var user  = userMapper.toEntity(request);
+        var user = userMapper.toEntity(request);
         userRepository.save(user);
         var userDto = userMapper.userToUserDto(user);
 
@@ -62,24 +73,24 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserDto> updateUser(@PathVariable(name = "id") Long id, @RequestBody UpdateUserRequest request){
-
+    public ResponseEntity<UserDto> updateUser(
+            @PathVariable(name = "id") Long id,
+            @RequestBody UpdateUserRequest request
+    ) {
         var user = userRepository.findById(id).orElse(null);
-        if(user == null){
+        if (user == null) {
             return ResponseEntity.notFound().build();
         }
-
         userMapper.update(request, user);
         userRepository.save(user);
 
         return ResponseEntity.ok(userMapper.userToUserDto(user));
-
     }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<UserDto> delete_user(@PathVariable(name = "id") Long id){
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserDto> delete_user(@PathVariable(name = "id") Long id) {
         var user = userRepository.findById(id).orElse(null);
-        if(user == null){
+        if (user == null) {
             return ResponseEntity.notFound().build();
         }
         userRepository.delete(user);
@@ -87,13 +98,15 @@ public class UserController {
     }
 
     @PostMapping("/{id}/change-password")
-    public ResponseEntity<Void> changePassword(@PathVariable(name = "id") Long id, @RequestBody ChangePasswordRequest request){
-
+    public ResponseEntity<Void> changePassword(
+            @PathVariable(name = "id") Long id,
+            @RequestBody ChangePasswordRequest request
+    ) {
         var user = userRepository.findById(id).orElse(null);
-        if(user == null){
+        if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        if(!user.getPassword().equals(request.getOldPassword())){
+        if (!user.getPassword().equals(request.getOldPassword())) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 //        userMapper.changePassword(request, user);   this works too
@@ -101,8 +114,8 @@ public class UserController {
         userRepository.save(user);
 
         return ResponseEntity.noContent().build();
-
     }
+
 
 
 }
---
+++ src/main/java/com/codewithmosh/store/validation/Lowercase.java
@@ -0,0 +1,20 @@
+package com.codewithmosh.store.validation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+//this is just a declartion of @lowercase annotation, the logic goes in another class
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = LowercaseValidator.class)
+public @interface Lowercase {
+    String message() default "must be lowercase";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
